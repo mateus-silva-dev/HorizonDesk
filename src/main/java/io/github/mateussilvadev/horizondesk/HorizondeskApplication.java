f65e6ffc56ebd7538b86f03305090a414c095437
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-07-13 17:06:15 -0300

    feat: Initialize project structure

---
+++ src/main/java/io/github/mateussilvadev/horizondesk/HorizondeskApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateussilvadev.horizondesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizondeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizondeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateussilvadev/horizondesk/HorizondeskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateussilvadev.horizondesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizondeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
