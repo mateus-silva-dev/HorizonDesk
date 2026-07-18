8bf0d4506eb1ed1773c011349b2e0f27da77e234
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-07-18 19:07:28 -0300

    feat(department): Added an endpoint to return a page containing all sectors, and an endpoint to return a list of all active sectors

--- src/main/java/io/github/mateussilvadev/horizondesk/controller/DepartmentController.java
+++ src/main/java/io/github/mateussilvadev/horizondesk/controller/DepartmentController.java
@@ -3,34 +3,39 @@ package io.github.mateussilvadev.horizondesk.controller;
 import io.github.docflowlib.docflow.annotations.ApiDocController;
 import io.github.docflowlib.docflow.annotations.ApiDocGet;
 import io.github.docflowlib.docflow.annotations.ApiDocPost;
-import io.github.mateussilvadev.horizondesk.dto.request.DepartmentCreateDTO;
-import io.github.mateussilvadev.horizondesk.dto.response.DepartmentResponseDTO;
+import io.github.mateussilvadev.horizondesk.dto.request.DepartmentRequestDTOs;
+import io.github.mateussilvadev.horizondesk.dto.response.DepartmentResponseDTOs;
+import io.github.mateussilvadev.horizondesk.dto.response.PageResponse;
 import io.github.mateussilvadev.horizondesk.mapper.DepartmentMapper;
 import io.github.mateussilvadev.horizondesk.model.domain.Department;
 import io.github.mateussilvadev.horizondesk.service.DepartmentService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/departments")
+@RequestMapping("/api/v1/departments")
 @ApiDocController
 public class DepartmentController {
 
-    private final DepartmentService departmentService;
+    private final DepartmentService service;
 
-    public DepartmentController(DepartmentService departmentService) {
-        this.departmentService = departmentService;
+    public DepartmentController(DepartmentService service) {
+        this.service = service;
     }
 
     @PostMapping
     @ApiDocPost
-    public ResponseEntity<DepartmentResponseDTO> create(@RequestBody DepartmentCreateDTO dto) {
-        Department savedDepartment = departmentService.create(dto);
-        DepartmentResponseDTO response = DepartmentMapper.toResponse(savedDepartment);
+    public ResponseEntity<DepartmentResponseDTOs.Response> create(@RequestBody DepartmentRequestDTOs.Create dto) {
+        Department savedDepartment = service.create(dto);
+        DepartmentResponseDTOs.Response response = DepartmentMapper.toResponse(savedDepartment);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{uuid}")
                 .buildAndExpand(response.uuid())
@@ -40,9 +45,27 @@ public class DepartmentController {
 
     @GetMapping("/{uuid}")
     @ApiDocGet
-    public ResponseEntity<DepartmentResponseDTO> getByUUID(@PathVariable UUID uuid) {
-        Department department = departmentService.findByUuid(uuid);
+    public ResponseEntity<DepartmentResponseDTOs.Response> getByUUID(@PathVariable UUID uuid) {
+        Department department = service.findByUuid(uuid);
         return ResponseEntity.ok(DepartmentMapper.toResponse(department));
+    }
+
+    @GetMapping
+    @ApiDocGet
+    public ResponseEntity<PageResponse<DepartmentResponseDTOs.Response>> getAll(
+            @PageableDefault(size = 20, sort = "name") Pageable page) {
+        Page<Department> pagedDepartments = service.findAll(page);
+        return ResponseEntity.ok(DepartmentMapper.toPageResponse(pagedDepartments));
+    }
+
+    @GetMapping("/options")
+    @ApiDocGet
+    public ResponseEntity<List<DepartmentResponseDTOs.Options>> getActiveOptions() {
+        List<Department> departments = service.findAllActiveOptions();
+        return ResponseEntity.ok(
+                departments.stream()
+                .map(DepartmentMapper::toOption)
+                .toList());
     }
 
 
