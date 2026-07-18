3ff3dac28d03641faf28e1826cd8838f2285ea97
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-07-18 19:11:35 -0300

    feat(department): new methods to retrieve active departments ordered by name

--- src/main/java/io/github/mateussilvadev/horizondesk/repository/DepartmentRepository.java
+++ src/main/java/io/github/mateussilvadev/horizondesk/repository/DepartmentRepository.java
@@ -3,10 +3,12 @@ package io.github.mateussilvadev.horizondesk.repository;
 import io.github.mateussilvadev.horizondesk.model.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DepartmentRepository extends JpaRepository<Department, UUID> {
     Optional<Department> findByUuid(UUID uuid);
     boolean existsByName(String name);
+    List<Department> findAllByActiveTrueOrderByNameAsc();
 }
